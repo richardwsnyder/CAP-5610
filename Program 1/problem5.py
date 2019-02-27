06f839c3f8b7dcc9d37c35dbb6eec4668557778d
richardwsnyder <richard.snyder@knights.ucf.edu>
2019-02-26 19:50:01 -0500

    changed problem5 with updated comments

--- Program 1/problem5.py
+++ Program 1/problem5.py
@@ -1,5 +1,6 @@
-# utilizing the union-find algorithms defined by 
+# utilizing the union-find and dfs algorithms defined by 
 # Arup Guha at http://www.cs.ucf.edu/~dmarino/ucf/cop3503/sampleprogs/djset.java
+# and geeksforgeeks at https://www.geeksforgeeks.org/depth-first-search-or-dfs-for-a-graph/
 
 import numpy as np
 import tensorflow as tf
@@ -18,9 +19,6 @@ parents = [0] * 784
 
 # load mnist data
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
-
-# number of images, rows and columns in each image
-image_count, rows, cols = x_train.shape
 
 # normalize the dataset so that pixel values are between
 # 0 and 1
@@ -45,9 +43,9 @@ def find(v):
 	return res;
 
 # run dfs on the image
+# i represents the current row
+# and j represents the current column in the image x
 def dfs(x, i, j):
-	global visited
-	global parents
 
 	# create two disparity vectors that correspond
 	# to the differnece in position. Will correlate to 
@@ -57,47 +55,50 @@ def dfs(x, i, j):
 
 	# index of current pixel in the visited and 
 	# parents array
-	current_pixel = (i * rows) + j
+	px = (i * rows) + j
 
-	# you're at the image, so set it's visited 
+	# you're at the pixel, so set it's visited 
 	# boolean to true
-	visited[current_pixel] = 1
+	visited[px] = 1
 
 	# if the pixel is black, then set the parent
 	# value to -1 because it is not considered
 	# a white region
 	if x[i][j] == 1:
-		parents[current_pixel] = -1
+		parents[px] = -1
 		return
 
 	# else, you must go to the pixel to the left, right
 	# up, and down from it
 	for n in range(4):
+		# calculate new x value
 		new_x = j + dx[n]
+		# calculate new y value
 		new_y = i + dy[n]
 
 		# get the next pixel that you're moving to
-		next_pixel = (i * rows) + j + (cols * dy[n]) + dx[n]
+		nx = (i * rows) + j + (cols * dy[n]) + dx[n]
 
 		# if the new pixel is in bounds and it's black, 
-		# set the parents cell to -1
+		# set the parents cell to -1, go onto next iteration
+		# of the loop
 		if 0 <= new_y < rows and 0 <= new_x < cols:
 			if x[new_y][new_x] == 1:
-				parents[next_pixel] = -1
+				parents[nx] = -1
 
 		# else, if you haven't visited the pixel and it's a white 
-		# pixel, run dfs on that
+		# pixel, find it's parent, and recursively call dfs on that
 		if 0 <= new_y < rows and 0 <= new_x < cols:
-			if x[new_y][new_x] == 0 and visited[next_pixel] == 0:
-				parents[next_pixel] = find(current_pixel)
+			if x[new_y][new_x] == 0 and visited[nx] == 0:
+				parents[nx] = find(px)
 				dfs(x, new_y, new_x)
 
-# run dfs on all pixels, so long as they haven't been visited
+# run dfs from top left pixel
 def djset(x):
 	for i in range(28):
 		for j in range(28):
-			current_pixel = (i * rows) + j
-			if visited[current_pixel] == 0:
+			px = (i * rows) + j
+			if visited[px] == 0:
 				dfs(x, i, j)
 
 	return len(np.unique(parents)) - 1
