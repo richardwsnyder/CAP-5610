d109edfe5a8858d87aea017b992627d512477906
richardwsnyder <richard.snyder@knights.ucf.edu>
2019-02-27 12:49:02 -0500

    change the labels to a vector of 0's and 1's

--- Program 1/problem3.py
+++ Program 1/problem3.py
@@ -4,14 +4,17 @@ import math
 import matplotlib.pyplot as plt
 import random
 from tensorflow.keras.datasets import mnist
+from tensorflow.keras.utils import to_categorical
 
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-x_train = x_train.reshape((60000, 784, 1))
-x_test = x_test.reshape((10000, 784, 1))
+x_train = x_train.reshape(60000, 784)
+x_test = x_test.reshape(10000, 784)
 
 x_train.astype('float32') / 255
 x_test.astype('float32') / 255
 
-print(y_test)
-print(y_test.shape)
+y_train = to_categorical(y_train)
+y_test = to_categorical(y_test)
+
+np.random.seed()
