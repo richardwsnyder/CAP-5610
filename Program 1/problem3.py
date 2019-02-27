cc94d334270f94fcabef4cf4a6b5050b86fb4455
richardwsnyder <richard.snyder@knights.ucf.edu>
2019-02-27 13:55:09 -0500

    more progress on training function

--- Program 1/problem3.py
+++ Program 1/problem3.py
@@ -37,15 +37,30 @@ b = bias
 def calculateZ(w, x, b):
 	return w.T.dot(x) + b
 
+# pass in the array z that contains the 10 calculated 
+# values from calculateZ()
+def softmax(z, k):
+	# take the max of the array z
+	z_max = z.max()
+	z2 = z - z_max
+	return np.exp(z[k]) / z.sum()
+
+# training function with no application of loss of gradient 
+# descent, just trying to figure out loss equation
 def train(learning_rate):
 	global w
 	global b
 	z = np.zeros((10, 1))
+	a = np.zeros((10, 1))
 	x_i = x_train[0]
 	y_i = y_train[0]
 	for j in range(10):
 		z[j] = calculateZ(w.T[j], x_i, b[j])
-		print(z[j])
+	for k in range(10):
+		a[k] = softmax(z, k)
+	print(np.argmax(a))
+			
 
 
 train(0.5)
+print(y_train[0])
