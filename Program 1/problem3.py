06236f624d28017b9e9559f6d0adbe84d6cfe9b9
richardwsnyder <richard.snyder@knights.ucf.edu>
2019-02-27 14:32:35 -0500

    did some stuff to train() method

--- Program 1/problem3.py
+++ Program 1/problem3.py
@@ -14,6 +14,7 @@ x_test = x_test.reshape(10000, 784)
 x_train.astype('float32') / 255
 x_test.astype('float32') / 255
 
+# change to categorical encoding for loss function
 y_train = to_categorical(y_train)
 y_test = to_categorical(y_test)
 
@@ -45,22 +46,68 @@ def softmax(z, k):
 	z2 = z - z_max
 	return np.exp(z[k]) / z.sum()
 
-# training function with no application of loss of gradient 
-# descent, just trying to figure out loss equation
+# cross entropy loss function
+def cat_cross_ent(y, a):
+	loss = 0.0
+	for k in range(10):
+		loss += y[k] * np.log(a[k])
+
+	return -1 * loss
+
+# partial derivative of L with respect to 
+# z_j
+def partialLwrtZ(y, a, j):
+	sum = 0.0
+	for k in range(10):
+		kroneker = 0
+		if j == k:
+			kroneker = 1
+		sum += y[k] * (a[j] - kroneker)
+
+	return sum
+
+# partial derivative of L with respect to 
+# w_ji
+def partialLwrtW(y, a, j, x):
+	return partialLwrtZ(y, a, j) * x
+
+# started working on gradient descent
 def train(learning_rate):
 	global w
 	global b
 	z = np.zeros((10, 1))
 	a = np.zeros((10, 1))
-	x_i = x_train[0]
-	y_i = y_train[0]
-	for j in range(10):
-		z[j] = calculateZ(w.T[j], x_i, b[j])
-	for k in range(10):
-		a[k] = softmax(z, k)
-	print(np.argmax(a))
-			
+	for i in range(60000):
+		x_i = x_train[0]
+		y_i = y_train[0]
+		for j in range(10):
+			z[j] = calculateZ(w.T[j], x_i, b[j])
+		for k in range(10):
+			a[k] = softmax(z, k)
+		for j in range(10):
+			w.T[j] -= learning_rate * partialLwrtW(y_i, a, j, x_i)
+	
+def test():
+	global w
+	global b
 
+	count = 0
+	z = np.zeros((10, 1))
+	a = np.zeros((10, 1))
+	for i in range(10000):
+		x = x_test[i]
+		y = y_test[i]
+		for j in range(10):
+			z[j] = calculateZ(w.T[j], x, b[j])
+		for k in range(10):
+			a[k] = softmax(z, k)
+		if np.argmax(a) == np.argmax(y):
+			count += 1
+	print("Accuracy - %0.4f" % ((count / 10000) * 100))
 
 train(0.5)
-print(y_train[0])
+print("done training")
+test()
+
+
+
