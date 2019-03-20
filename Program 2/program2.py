7f5b5cd64019b83d02eac16c77eb88f36cf55b00
Richard Snyder <richard.snyder@knights.ucf.edu>
2019-03-20 14:25:46 -0400

    checking credentials

--- Program 2/program2.py
+++ Program 2/program2.py
@@ -2,4 +2,5 @@ from tensorflow.keras.datasets import cifar10
 import matplotlib.pyplot as plt
 (x_train, y_train), (x_test, y_test) = cifar10.load_data()
 
+# define the class names
 class_names = ['airplan', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
