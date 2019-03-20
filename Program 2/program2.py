f08841fc2d108747dff660674029b0a63427aa48
Richard Snyder <richard.snyder@knights.ucf.edu>
2019-03-20 14:23:31 -0400

    added Program 2 directory

---
+++ Program 2/program2.py
@@ -0,0 +1,5 @@
+from tensorflow.keras.datasets import cifar10
+import matplotlib.pyplot as plt
+(x_train, y_train), (x_test, y_test) = cifar10.load_data()
+
+class_names = ['airplan', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
