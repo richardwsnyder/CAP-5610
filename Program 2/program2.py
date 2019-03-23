653f9e7f9eadf0ae28668dff3cbfbe2fa641e191
Richard Snyder <richard.snyder@knights.ucf.edu>
2019-03-23 16:34:17 -0400

    introduce k-folds

--- Program 2/program2.py
+++ Program 2/program2.py
@@ -10,9 +10,31 @@ class_names = ['airplan', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'h
 
 # create the number of folds for k-fold 
 # validation
-k_folds = 5
+k_folds = 10
 
 # shuffle the training data
 p = np.random.permutation(50000)
 x_train_shuffled = x_train[p]
 y_train_shuffled = y_train[p]
+
+# size of validation block based off
+# of number of folds
+size_validation = x_train_shuffled.shape[0] / k_folds
+
+x_validation_list = []
+y_validation_list = []
+
+x_training_list = []
+y_training_list = []
+
+# append the different validation sets to their
+# respective validation lists
+for i in range(k_folds):
+	x_validation_list.append(x_train_shuffled[(i * size_validation) : ((i + 1) * size_validation)])
+	y_validation_list.append(y_train_shuffled[(i * size_validation) : ((i + 1) * size_validation)])
+
+# append the different validation sets to their
+# respective validation lists
+for i in range(k_folds):
+	x_training_list.append(x_train_shuffled[((i - 1) * size_validation) : ])
+	y_training_list.append(y_train_shuffled[(i * size_validation) : ((i + 1) * size_validation)])
